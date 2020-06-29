0baf77aa3856d4da749a39ba5cada4380e8fc603
Lucie Grand <luciemylene.grand@gmail.com>
2020-06-29 16:49:47 +0200

    token

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    const decodedToken = jwt.verify(token, '8c1a23d4e1e5263dad0135c8aa32e95b4f8c8f7c7a9af0b5557f521f3de97ae1');
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
